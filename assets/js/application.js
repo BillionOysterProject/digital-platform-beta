6ff0a0ccd25113eb3670cbc642b32654d00d11ca
Gary Hetzel <garyhetzel@gmail.com>
2017-12-04 21:58:19 -0500

    Init import

---
+++ assets/js/application.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var App = Stapes.class({
+    constructor: function() {
+    },
+
+    authenticate: function(el) {
+        var form = $(el);
+        var username = form.find('input[name="username"]').val();
+        var password = form.find('input[name="password"]').val();
+
+        $.ajax({
+            url:    form.attr('action'),
+            method: form.attr('method'),
+            data: {
+                'username': username,
+                'password': password,
+            },
+            success: function() {
+                window.location = '/';
+            },
+            error: function(data) {
+                console.error('Failed', data);
+            },
+        })
+
+        return false;
+    },
+});
+
+var bop = App();
