b506a431e958c4684982adfa7fab7902ba7f7762
Gary Hetzel <garyhetzel@gmail.com>
2017-12-10 17:48:46 -0500

    Switching to bootstrap 4

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,31 +1,33 @@
 'use strict';
 
-var App = Stapes.class({
-    constructor: function() {
-    },
+$(function(){
+    var App = Stapes.subclass({
+        constructor: function() {
+        },
 
-    authenticate: function(el) {
-        var form = $(el);
-        var username = form.find('input[name="username"]').val();
-        var password = form.find('input[name="password"]').val();
+        authenticate: function(el) {
+            var form = $(el);
+            var username = form.find('input[name="username"]').val();
+            var password = form.find('input[name="password"]').val();
 
-        $.ajax({
-            url:    form.attr('action'),
-            method: form.attr('method'),
-            data: {
-                'username': username,
-                'password': password,
-            },
-            success: function() {
-                window.location = '/';
-            },
-            error: function(data) {
-                console.error('Failed', data);
-            },
-        })
+            $.ajax({
+                url:    form.attr('action'),
+                method: form.attr('method'),
+                data: {
+                    'username': username,
+                    'password': password,
+                },
+                success: function() {
+                    window.location = '/';
+                },
+                error: function(data) {
+                    console.error('Failed', data);
+                },
+            })
 
-        return false;
-    },
+            return false;
+        },
+    });
+
+    var bop = new App();
 });
-
-var bop = App();
