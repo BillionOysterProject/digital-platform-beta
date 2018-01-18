03e1b9f16af331cbabb415f4a676035305cb498d
Gary Hetzel <garyhetzel@gmail.com>
2018-01-17 19:08:41 -0500

    working on gevent wsgi server

--- backend/server.py
+++ backend/server.py
@@ -2,9 +2,19 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from bop import API
+import os
+import logging
+from gevent.wsgi import WSGIServer
+
 
 app = API('bop-digital-platform')
+addr = os.environ.get('HTTP_HOST', '127.0.0.1')
+port = int(os.environ.get('HTTP_PORT', 5000))
 
+logging.warning('Running {} on http://{}:{}'.format(app.name, addr, port))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__' and not int(os.environ.get('WSGI', '0')):
+    app.run(debug=False, host=addr, port=port)
+else:
+    http_server = WSGIServer((addr, port), app)
+    http_server.serve_forever()
