9e94ecfdcdc910132bee600d8a320e515075e537
Gary Hetzel <garyhetzel@gmail.com>
2018-03-14 11:39:33 -0400

    update-latlng

--- scripts/data/org-sync/syncProspectiveToSchoolOrgs.py
+++ scripts/data/org-sync/syncProspectiveToSchoolOrgs.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+from __future__ import unicode_literals
 import pivot
 import csv
 import sys
@@ -25,7 +26,11 @@ for school in schoolOrgs.query('all', limit=False):
     if school.syncId:
         has_changed = False
 
-        aProspectiveOrg = prospectiveOrgs.query('syncId/{}'.format(school.syncId), limit=1)
+        try:
+            aProspectiveOrg = prospectiveOrgs.query('syncId/{}'.format(school.syncId), limit=1).records[0]
+        except IndexError:
+            print('Unchanged: {}'.format(school.id))
+            continue
 
         for k, v in school.items():
             if k in SKIP_FIELDS:
@@ -35,10 +40,23 @@ for school in schoolOrgs.query('all', limit=False):
                 if school[k] != aProspectiveOrg[k]:
                     has_changed = True
                     school[k] = aProspectiveOrg[k]
+                    print('  [U] {}={}'.format(k, school[k]))
+
+        for k in [
+            'latitude',
+            'longitude',
+        ]:
+            v = aProspectiveOrg.get(k)
+
+            if v and school.get(k) != v:
+                school[k] = v
+                has_changed = True
+                print('  [U] {}={}'.format(k, school[k]))
 
         if has_changed:
             print('   Synced: {} ({})'.format(school.id, school.name))
             schoolOrgs.update(school)
         else:
             print('Unchanged: {}'.format(school.id))
+
 
